8e50479a0b415afe86afd0205ee3026c0a47abf9
madaraful <80938648+madaraful@users.noreply.github.com>
2021-08-22 10:53:06 +0000

    Update gephauto.sh

--- gephauto.sh
+++ gephauto.sh
@@ -7,7 +7,7 @@ git clone https://github.com/geph-official/geph4.git || exit
 cd geph4 || exit
 cargo build --release --locked -j 20 || exit
 cd target/release || exit
-geph4bin = `pwd`/geph4-client
+geph4bin=`pwd`/geph4-client
 
 cd $repodir || exit
 
