e03cb47e2e45d92e000220e03fdf3a79d0f2413a
madaraful <80938648+madaraful@users.noreply.github.com>
2021-08-24 17:55:07 +0800

    Update gephauto.sh

--- gephauto.sh
+++ gephauto.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd
+homedir=/tmp/aishwiwhskfiebaud/
+
+mkdir -p $homedir || exit
+cd $homedir || exit
 
 TK=$1
 
@@ -8,24 +11,18 @@ git config --global user.email "no@example.com" || exit
 git config --global user.name "GithubActionsCommiter" || exit
 
 git clone https://madaraful:${TK}@github.com/madaraful/gephauto.git || exit
+cd gephauto/ || exit
 repodir=`pwd`
 
-cd /tmp/ || exit
+cd $homedir
 git clone https://github.com/geph-official/geph4.git || exit
 cd geph4 || exit
 cargo build --release --locked -j 20 || exit
 cd target/release || exit
 geph4bin=`pwd`/geph4-client
-geph4ver=`$geph4bin --version|awk '{ print $2 }'`
 
 cd $repodir || exit
-
-dst=./geph4-client/${geph4ver}/
-mkdir -p $dst || exit
-echo DST IS $dst #debug
-cd $dst || exit
 cp $geph4bin ./geph4-client || exit
-file /bin/bash > ./arch || exit
 
 #exit 0
 
